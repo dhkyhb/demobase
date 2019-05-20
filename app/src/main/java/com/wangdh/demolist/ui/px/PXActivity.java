10fafce18a3aeaedb043abc8295b386d4ba6e909
wangdh <243823965@qq.com>
2019-05-20 09:23:16 +0800

    测试错误

--- app/src/main/java/com/wangdh/demolist/ui/px/PXActivity.java
+++ app/src/main/java/com/wangdh/demolist/ui/px/PXActivity.java
@@ -6,7 +6,8 @@ import android.view.View;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 import com.wangdh.dblibrary.DBTest;
 import com.wangdh.demolist.R;
-import com.wangdh.netlibrary.test.NetTest;
+import com.wangdh.netlibrary.NetTest;
+
 
 /**
  * 测试  hdip  mdip ldip 图片的
@@ -19,10 +20,10 @@ public class PXActivity extends RxAppCompatActivity {
         setContentView(R.layout.activity_px);
         DBTest.insert();
 //        NetTest._3(this);
-        NetTest._3(this);
+        NetTest.conn_xiao_hua(this);
     }
 
     public void click(View view) {
-        NetTest._3(null);
+        NetTest.conn_xiao_hua(null);
     }
 }
