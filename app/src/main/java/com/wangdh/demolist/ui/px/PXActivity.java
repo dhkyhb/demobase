7ed8bcaf4ae88423f36acd33cbccd317f81e16c3
wangdh <243823965@qq.com>
2019-07-27 09:30:47 +0800

    同步

--- app/src/main/java/com/wangdh/demolist/ui/px/PXActivity.java
+++ app/src/main/java/com/wangdh/demolist/ui/px/PXActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 import com.wangdh.dblibrary.DBTest;
 import com.wangdh.demolist.R;
-import com.wangdh.netlibrary.test.NetTest;
+
 
 /**
  * 测试  hdip  mdip ldip 图片的
@@ -18,11 +18,11 @@ public class PXActivity extends RxAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_px);
         DBTest.insert();
-//        NetTest._3(this);
-        NetTest._3(this);
+
+
     }
 
     public void click(View view) {
-        NetTest._3(null);
+
     }
 }
