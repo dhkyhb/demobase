5521a53a48256adcb2ae41c291ffcc41d8d5ce44
wangdh <243823965@qq.com>
2019-08-19 16:32:44 +0800

    1

--- app/src/main/java/com/wangdh/demolist/AppConfig.java
+++ app/src/main/java/com/wangdh/demolist/AppConfig.java
@@ -49,14 +49,14 @@ public class AppConfig {
     public final static String DEFAULT_SAVE_IMAGE_PATH = Environment
             .getExternalStorageDirectory()
             + File.separator
-            + "OSChina"
+            + "wangdh"
             + File.separator + "osc_img" + File.separator;
 
     // 默认存放文件下载的路径
     public final static String DEFAULT_SAVE_FILE_PATH = Environment
             .getExternalStorageDirectory()
             + File.separator
-            + "OSChina"
+            + "wangdh"
             + File.separator + "download" + File.separator;
 
     private Context mContext;
